0ebc6b4134947be6b01e65150559a4a9a8bcbe29
Zebio <mateuseusebio@gmail.com>
2022-05-04 16:54:48 -0300

    Adição do script python

--- main.cpp
+++ main.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 #include <fstream> //manipulacao de arquivos
 #include <vector>
-#include<math.h> //pow
-#include<direct.h>
+#include <math.h> //pow
+#include <direct.h>
+#include <cstring>
 
 using namespace std;
-
-
-
 
 int main()
 {
@@ -17,7 +15,7 @@ int main()
 
 
     ifstream arquivo_da_tabela;
-    arquivo_da_tabela.open("tabela_verdade.txt");
+    arquivo_da_tabela.open("tabela_verdade_of.txt");
     string buffer;
     if (arquivo_da_tabela.is_open())
     {
@@ -94,11 +92,11 @@ int main()
 
     for(int s=0; s<numero_de_saidas;s++)
     {
-        output_false_expression=saidas[s];
-        output_false_expression.append("_f=");
+        //output_false_expression=saidas[s];
+        //output_false_expression.append("_f=");
 
-        output_true_expression=saidas[s];
-        output_true_expression.append("_t=");
+        //output_true_expression=saidas[s];
+        //output_true_expression.append("_t=");
         bool primeiro_false=1;
         bool primeiro_true=1;
         for(int i=0;i<linhas;i++)
@@ -108,20 +106,20 @@ int main()
             {
                 if(!primeiro_false)
                 {
-                    output_false_expression.append(" | ");
+                    output_false_expression.append(" ");
                 }
                 primeiro_false=0;
 
                 for(int j=0; j<numero_de_entradas;j++)
                 {
-                    output_false_expression.append(entradas[j]);
+                    //output_false_expression.append(entradas[j]);
                     if(matriz[i][j]==false){
-                        output_false_expression.append("_f");
+                        output_false_expression.append("0");
                     }    
                     else
-                        output_false_expression.append("_t");
-                    if (j+1<numero_de_entradas)
-                        output_false_expression.append(" & ");
+                        output_false_expression.append("1");
+                    //if (j+1<numero_de_entradas)
+                        //output_false_expression.append(" & ");
                 }
                 
                 
@@ -131,27 +129,173 @@ int main()
             {
                 if(!primeiro_true)
                 {
-                    output_true_expression.append(" | ");
+                    output_true_expression.append(" ");
                 }
                 primeiro_true=0;
 
 
                 for(int j=0; j<numero_de_entradas;j++)
                 {
-                    output_true_expression.append(entradas[j]);
+                    //output_true_expression.append(entradas[j]);
                     if(matriz[i][j]==false){
-                        output_true_expression.append("_f");
+                        output_true_expression.append("0");
                     }    
                     else
-                        output_true_expression.append("_t");
-                    if (j+1<numero_de_entradas)
-                        output_true_expression.append(" & ");
+                        output_true_expression.append("1");
+                    //if (j+1<numero_de_entradas)
+                      //  output_true_expression.append(" & ");
                 }
             }
         }
     }
     cout<<"\ntrue exp: "<<output_true_expression<<endl;
+    ofstream true_exp;
+    true_exp.open("str_terms_file.txt");
+    true_exp<<output_true_expression;
+    true_exp.close();
+
+    system("python3 \"c:\\Users\\mateu\\Documents\\kmap\\Kmap\\Kmap.py\"");
+    //python script
+
+    ifstream simp_true_exp;
+    simp_true_exp.open("str_terms_file_output.txt");
+    string buffer2;
+    getline(simp_true_exp,buffer2);
+    simp_true_exp.close();
+    cout<<"buffer: "<<buffer2<<endl;
+    int n = buffer2.length();
+    char char_array[n + 1];
+    strcpy(char_array, buffer2.c_str());
+    int contador=0;
+    bool first=true;
+    output_true_expression= saidas[0];
+    output_true_expression.append("_t= ");
+    for(int i=0; i<n; i++)
+    {
+        //cout<<"\nchar array test, i="<<i<<"char_array[i]="<<char_array[i];
+        if(char_array[i]=='0')
+        {
+            if(!first)
+            {
+                output_true_expression.append(" & ");
+                
+            }
+            else{
+                first = false;
+            }
+            output_true_expression.append(entradas[contador]);
+            output_true_expression.append("_f");
+            contador++;
+        }
+        else if(char_array[i]=='1')
+        {
+            if(!first)
+            {
+                output_true_expression.append(" & ");
+                
+            }
+            else{
+                first = false;
+            }
+            output_true_expression.append(entradas[contador]);
+            output_true_expression.append("_t"); 
+            contador++;
+        }
+        else if(char_array[i]==' ')
+        {
+            contador=0;
+            output_true_expression.append(" | "); 
+            first=true;
+        }
+        else
+        {
+            contador++;
+        }
+    }
+    output_true_expression.pop_back();
+    output_true_expression.pop_back();
+    cout<<"\ntrue exp: "<<output_true_expression<<endl;
+    
+
+
+
+
+
+
+
+
     cout<<"\nfalse exp: "<<output_false_expression<<endl;
+
+    ofstream false_exp;
+    false_exp.open("str_terms_file.txt");
+    false_exp<<output_false_expression;
+    false_exp.close();
+
+    system("python3 \"c:\\Users\\mateu\\Documents\\kmap\\Kmap\\Kmap.py\"");
+    //python script
+
+    ifstream simp_false_exp;
+    simp_false_exp.open("str_terms_file_output.txt");
+    buffer2 = "";
+    getline(simp_false_exp,buffer2);
+    simp_false_exp.close();
+    cout<<"buffer: "<<buffer2<<endl;
+    n = buffer2.length();
+    char char_array2[n + 1];
+    strcpy(char_array2, buffer2.c_str());
+    contador=0;
+    first=true;
+    output_false_expression= saidas[0];
+    output_false_expression.append("_f= ");
+    for(int i=0; i<n; i++)
+    {
+        //cout<<"\nchar array test, i="<<i<<"char_array[i]="<<char_array[i];
+        if(char_array2[i]=='0')
+        {
+            if(!first)
+            {
+                output_false_expression.append(" & ");
+                
+            }
+            else{
+                first = false;
+            }
+            output_false_expression.append(entradas[contador]);
+            output_false_expression.append("_f");
+            contador++;
+        }
+        else if(char_array2[i]=='1')
+        {
+            if(!first)
+            {
+                output_false_expression.append(" & ");
+                
+            }
+            else{
+                first = false;
+            }
+            output_false_expression.append(entradas[contador]);
+            output_false_expression.append("_t"); 
+            contador++;
+        }
+        else if(char_array2[i]==' ')
+        {
+            contador=0;
+            output_false_expression.append(" | "); 
+            first=true;
+        }
+        else
+        {
+            contador++;
+        }
+    }
+    output_false_expression.pop_back();
+    output_false_expression.pop_back();
+    cout<<"\nfalse exp: "<<output_false_expression<<endl;
+
+
+
+
 
     string entradas_dual_rail;
     string histerese;
@@ -193,8 +337,8 @@ int main()
     verilog_module << "module "<<nome_do_modulo<< "(input " <<entradas_dual_rail<<endl;
     verilog_module << "output "<<saidas_dual_rail<<");"<<endl;
 
-    verilog_module <<" \n\nassign "<<output_true_expression<<" | (("<<histerese<<") & S_t)"  <<";\n";
-    verilog_module <<" \n\nassign "<<output_false_expression<<" | (("<<histerese<<") & S_f)"  <<";\n";
+    verilog_module <<" \n\nassign "<<output_true_expression<<" | (("<<histerese<<") & "<<saidas[0]<<"_t)"  <<";\n";
+    verilog_module <<" \n\nassign "<<output_false_expression<<" | (("<<histerese<<") & "<<saidas[0]<<"_f)"  <<";\n";
 
     verilog_module <<" \nendmodule "<<endl;
     verilog_module.close();
